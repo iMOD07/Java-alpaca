2d25ae24721f2d785dde0bcdf45b7e5420b7e706
iMOD07 <mo.2301@hotmail.com>
2025-09-28 22:03:04 +0300

    add dependencies

---
+++ src/main/java/com/alpaca/trading/TradingApplication.java
@@ -0,0 +1,14 @@
+package com.alpaca.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingApplication.class, args);
+        System.out.println("First Project");
+	}
+
+}
